ee59503a210757eb209195ef2d4b3fb516aaf322
pentesterjeff <36642753+pentesterjeff@users.noreply.github.com>
2018-02-20 12:09:05 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -8,7 +8,7 @@ try:
 	url = sys.argv[1]
 	ports = [20, 21, 22, 23, 25, 53, 67, 68, 80, 110, 123, 143, 156, 179, 442, 1723, 1863, 3128, 2289]
 except:
-	print 'Usege: python portscan.py <url>'
+	print 'Usege: python portscan.py <Address|IP>'
 	print 'Usage: ./portscan.py'
 	sys.exit()
 print
